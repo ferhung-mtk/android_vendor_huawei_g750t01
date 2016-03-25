bee01dd0eb8fe085d1c5a6a119f2d5c4f02fe40c
ferhung-mtk <ferhung27@gmail.com>
2016-03-25 20:38:36 +0800

    first commit.

---
+++ g750t01-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/g750t01/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/g750t01/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/g750t01/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/g750t01/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/huawei/g750t01/xbin,system/xbin)
+
